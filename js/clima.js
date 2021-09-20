6f453b8c5e4951cf588703e03d69bac293bc2a41
matheus-lauritzen <matheus_lauritzen@estudante.sc.senai.br>
2021-09-20 18:27:03 -0300

    Alteração do cache

--- js/clima.js
+++ js/clima.js
@@ -7,49 +7,33 @@ function getClima() {
         dataType: 'json',
 
         success: function (data) {
-            plotarResultados(data);
-            localStorage.clima = JSON.stringify(data);
-            localStorage.alteracaoCache = new Date().getTime();
+
+            console.log(data.weather[0].description);
+
+            let converterCelsius = (data.main.temp - 273.15);
+            celsius = (Math.round(converterCelsius) + "C°");
+            $('#temperatura').html(celsius);
+
+            $('#condicao').html(data.weather[0].description);
+
+            $('#vento').html(data.wind.speed + "m/s");
+            $('#umidade').html(data.main.humidity + "%");
+
+            $('#nascer').html(data.sys.sunset);
+            $('#sepor').html(data.sys.sunrise);
+
+            let icone = './icons/' + data.weather[0].icon  + '.png';
+            $('#i_condicao').attr('src',icone);
         },
 
         error: function (argument) {
             alert('Falha ao obter dados!');
         }
+
         
     });
 }
 
-function plotarResultados(data){
-    console.log(data.weather[0].description);
-
-            let converterCelsius = (data.main.temp - 273.15);
-            celsius = (Math.round(converterCelsius) + "C°");
-            $('#temperatura').html(celsius);
-            $('#condicao').html(data.weather[0].description);
-            $('#vento').html(data.wind.speed + "m/s");
-            $('#umidade').html(data.main.humidity + "%");
-
-            let icone = 'icons/' + data.weather[0].icon  + '.png';
-            $('#i_condicao').attr('src',icone);
-            $('#nascer').html(data.sys.sunrise);
-            $('#por').html(data.sys.sunset);
-}
-
-function getDadosClima(){
-
-    let tempoAtual = new Date().getTime();
-    let tempoCache = parseInt(localStorage.alteracaoCache);
-    let diferencaTempos = tempoAtual - tempoCache;
-
-    if (diferencaTempos > 300000){
-        getClima();
-    } else {
-        let data = JSON.parse(localStorage.clima);
-        plotarResultados(data);
-    }
-
-}
-
 window.onload = function () {
-    getDadosClima();
+    getClima();
 };
